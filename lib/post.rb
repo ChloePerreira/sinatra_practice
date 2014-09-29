4573ec6f26e6e6a97dfdb8a97ba4021869ffda76
Crystal Perreira <cvperreira@gmail.com>
2014-09-28 20:51:36 -0700

    first commit

--- lib/post.rb
+++ lib/post.rb
@@ -6,14 +6,38 @@ class Post
   # Create an array of post objects with attributes set
   # Post.all =? [post, post]
 
-  def initialize(array)
-    
-    
+  attr_accessor :title, :url
+
+  def initialize(array) #add a date attribute @date and then change @url
+    @title = array[-1].split(".")[0]  
+    @url   = "posts/#{@title}" #interpolate title, leave out views (this is like the url href)
   end
 
   def self.all # class method that creates instances
-    Dir.glob("views/posts*").collect do |filepath| # use collect to return result of each itrtn
-      a = filepath.split("/")
+    Dir.glob("views/posts/*").collect do |filepath| # use collect to return result of each itrtn
+      a = filepath.split("/") # yields ["posts", "views", "blog.erb"]
       Post.new(a) #or new or self.new since in class method
+    end
   end
+
+  def print_date(title)
+    array = title.gsub(/_/, ' ').split(' ').reverse # y m d
+    year = array[0].to_i + 2000
+    month = array[2].to_i
+    case month 
+      when 9
+        month = "September"
+      when 10
+        month = "October"
+    end
+    day = array[1]
+    "#{month} #{day}, #{year}"
+  end
+
+
+  def self.most_recent(n)
+  end
+  
+    
+    
 end
--- my_app.rb
+++ my_app.rb
@@ -1,15 +1,28 @@
 require 'sinatra'
+require './lib/post.rb'
 
 class MyApp < Sinatra::Base
+
+  before do #this will make stuff available in every route handler
+    # e.g. @posts = Post.all
+  end
+
   get "/" do
+    @posts = Post.all # @ posts in same scope so available within home file!
     erb :home #links to recent posts
+  end
+
+  post "/" do
+    #this is where you do logic to database
+    puts params.inspect
+    redirect to("/") #redirect makes it so it doesnt show in url, but it puts in server running area
   end
 
   get "/about" do
     erb :about_me
   end
 
-  get "/posts/:blogposts" do #by date mm_dd_yy
+  get "/posts/:blogposts" do #by date mm_dd_yyi # can do posts/:date/:somethingelse
     erb "/posts/#{params[:blogposts]}".to_sym #dynamically find posts by date
     #("/posts/"params[:blogposts]).to_sym possibly work
   end
