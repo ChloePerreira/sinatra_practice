f2a6d71af7ba0aa89bfd526011c5c7b1b495b8ae
Crystal Perreira <cvperreira@gmail.com>
2014-10-02 16:12:21 -0700

    Add jQuery cycle to quotes

--- public/javascript.js
+++ public/javascript.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function(){
   $(".github img").hover(function() {
-    $(this).attr("src","/github-128-green.png");
+    $(this).attr("src","/github-128-green1.png");
       }, function() {
     $(this).attr("src","/github-128-black.png");
   });
   $(".linkedin img").hover(function() {
-    $(this).attr("src","/linkedin-128-green.png");
+    $(this).attr("src","/linkedin-128-green1.png");
       }, function() {
     $(this).attr("src","/linkedin-128-black.png");
   });
@@ -15,4 +15,13 @@ $(document).ready(function(){
       }, function() {
     $(this).attr("src","/twitter-128-black.png");
   });
+
+  var divs = $('div[id^="quote-"]').hide(),
+    i = 0;
+  (function cycle() { 
+    divs.eq(i).fadeIn(400)
+              .delay(7000)
+              .fadeOut(400, cycle);
+    i = ++i % divs.length;
+  })();
 });    
